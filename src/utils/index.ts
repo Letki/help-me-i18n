730a135dcc8908a6a4ae8c03695cd7a0ea962505
gaozhixiong <gaozhixiong@yy.com>
2021-02-04 21:04:38 +0800

    fix: 修复tsnode问题

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import * as child_process from 'child_process';
 import { Log } from '../utils/Log';
+import { cwd } from 'process';
 
 export const loadModuleData = async (filePath: string, extensionPath: string) => {
   const compilerOptions = {
@@ -11,16 +12,19 @@ export const loadModuleData = async (filePath: string, extensionPath: string) =>
     allowJs: true,
   };
   const loader = path.resolve(extensionPath!, 'assets/loader.js');
-  const tsNode = path.resolve(extensionPath!, 'assets/ts-node/dist/bin.js');
+  const tsNode = path.resolve(extensionPath!, 'node_modules/ts-node/dist/bin.js');
   const options = JSON.stringify(compilerOptions).replace(/"/g, '\\"');
   return new Promise<any>((resolve, reject) => {
     const cmd = `node "${tsNode}" --transpile-only --compiler-options "${options}" "${loader}" "${filePath}"`;
     // console.log(`[i18n-ally] spawn: ${cmd}`);
     child_process.exec(cmd, (err, stdout) => {
-      if (err) return reject(err);
+      if (err) {
+        Log.info(err.message)
+        return reject(err)};
       try {
         resolve(JSON.parse(stdout.trim()));
       } catch (e) {
+        Log.info(e.message)
         reject(e);
       }
     });
